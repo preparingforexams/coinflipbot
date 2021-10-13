4935a5858afbe9123930ae1a9fc2f428daa1fd18
Björn Petersen <git@bjoernpetersen.net>
2021-10-13 11:08:14 +0200

    Remove timezone-checking from use case

--- application/lib/src/use_case/send_attendance_poll.dart
+++ application/lib/src/use_case/send_attendance_poll.dart
@@ -1,10 +1,7 @@
 import 'package:application/port.dart' as port;
-import 'package:domain/config.dart';
 import 'package:domain/outbound_model.dart' as outbound_model;
 import 'package:injectable/injectable.dart';
 import 'package:logging/logging.dart';
-import 'package:timezone/data/latest_10y.dart' as tz_data;
-import 'package:timezone/timezone.dart' as tz;
 
 @injectable
 class SendAttendancePoll {
@@ -13,27 +10,8 @@ class SendAttendancePoll {
 
   SendAttendancePoll(this._pollPublisher);
 
-  Future<bool> _isHammerTime() async {
-    tz_data.initializeTimeZones();
-    final berlin = tz.getLocation('Europe/Berlin');
-
-    final now = tz.TZDateTime.now(berlin);
-    final target = tz.TZDateTime(
-      berlin,
-      now.year,
-      now.month,
-      now.day,
-      Config.schedule.attendanceHour,
-    );
-    final delta = Duration(minutes: 5);
-    return now.difference(target).abs() < delta;
-  }
-
   Future<void> call() async {
-    if (!await _isHammerTime()) {
-      _log.fine("Skipping attendance poll because it ain't hammer time.");
-      return;
-    }
+    _log.info('Sending attendance poll');
 
     // TODO send correct poll
     // TODO save poll ID somewhere
